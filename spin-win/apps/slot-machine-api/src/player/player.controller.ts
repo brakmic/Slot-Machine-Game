d81d9efca4642679abe6ca0e56f5183250aa38f9
Harris Brakmic (Extern) <92522482+brakmic-extern@users.noreply.github.com>
2023-07-25 09:58:01 +0200

    fix: use http put for player updates

--- spin-win/apps/slot-machine-api/src/player/player.controller.ts
+++ spin-win/apps/slot-machine-api/src/player/player.controller.ts
@@ -1,10 +1,27 @@
-import { Controller, Post, Body, Param, Get, Delete, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  Delete,
+  HttpException,
+  HttpStatus,
+  Put,
+} from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { CreatePlayerCommand, DepositMoneyCommand, DeletePlayerCommand } from '@player/commands/player.commands';
+import {
+  CreatePlayerCommand,
+  DepositMoneyCommand,
+  DeletePlayerCommand,
+} from '@player/commands/player.commands';
 import { ApiTags } from '@nestjs/swagger';
 import { CreatePlayerDto } from '@player/dto/create-player.dto';
 import { DepositMoneyDto } from '@player/dto/deposit-money.dto';
-import { GetAllPlayersQuery, GetPlayerByIdQuery } from '@player/queries/player.queries';
+import {
+  GetAllPlayersQuery,
+  GetPlayerByIdQuery,
+} from '@player/queries/player.queries';
 
 @ApiTags('players')
 @Controller('players')
@@ -26,7 +43,9 @@ export class PlayerController {
 
   @Post()
   async createPlayer(@Body() data: CreatePlayerDto) {
-    return await this.commandBus.execute(new CreatePlayerCommand(data.name, data.balance));
+    return await this.commandBus.execute(
+      new CreatePlayerCommand(data.name, data.balance)
+    );
   }
 
   @Delete(':id')
@@ -34,8 +53,10 @@ export class PlayerController {
     return await this.commandBus.execute(new DeletePlayerCommand(Number(id)));
   }
 
-  @Post(':id/deposit')
+  @Put(':id')
   async depositMoney(@Param('id') id: string, @Body() data: DepositMoneyDto) {
-    return await this.commandBus.execute(new DepositMoneyCommand(Number(id), data.amount));
+    return await this.commandBus.execute(
+      new DepositMoneyCommand(Number(id), data.amount)
+    );
   }
 }
